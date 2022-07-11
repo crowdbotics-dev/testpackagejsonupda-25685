d8c4fa9477020c88d1af87d4e2686fdb8eb17877
scartag <tag@crowdbotics.com>
2022-07-11 17:18:30 +0100

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={{
+    backgroundColor: '#f0f0f1',
+    padding: 10,
+    position: 'relative',
+    height: '100%'
+  }}>
+      <View style={{
+      left: 28,
+      top: 35,
+      position: "absolute",
+      height: 60,
+      width: 140,
+      backgroundColor: "#ca4b4b",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View></View>;
+};
+
+export default Untitled1;
